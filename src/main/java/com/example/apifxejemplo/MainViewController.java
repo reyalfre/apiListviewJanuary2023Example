0e72755c6af04b67a12f4668407531e4991336ac
ALFREDO MALDONADO <alfredorafael.maldonado@alumno.iesluisvives.org>
2023-01-09 11:58:08 +0100

    Finish class 9-1-2023

--- src/main/java/com/example/apifxejemplo/MainViewController.java
+++ src/main/java/com/example/apifxejemplo/MainViewController.java
@@ -3,6 +3,7 @@ package com.example.apifxejemplo;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
 import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -20,6 +21,24 @@ public class MainViewController {
         cliente.sendAsync(request, HttpResponse.BodyHandlers.ofString()).thenAccept(response -> {
                     if (response.statusCode() == 200) {
                         JSONArray dataArray = new JSONArray(response.body());
+                        for (int i=0 ;i<dataArray.length();i++){
+                            JSONObject data = dataArray.getJSONObject(i);
+                            //System.out.println(data.getJSONObject(i));
+                            var producto= new Product();
+                            producto.setId(data.getInt("id"));
+                            producto.setName(data.getString("title"));
+                            producto.setCategory(data.getString("category"));
+                            producto.setDescription(data.getString("description"));
+                            producto.setImage(data.getString("image"));
+                            producto.setPrice(data.getDouble("price"));
+                            listView.getItems().add(producto);
+                        }
+
+                        dataArray.forEach(data-> {
+                            System.out.println("data: "+data);
+                        });
+
+
                         System.out.println(response.body());
                     }
                 })
